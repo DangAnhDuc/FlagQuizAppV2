af78af6458dbf641b427e9249d83fc9f6428d7e9
DangAnhDuc <anhduc.dang@my.jcu.edu.au>
2018-12-15 03:00:23 +0800

    cac

---
+++ app/src/main/java/com/example/admin/flagquizapp/Comon/Common.java
@@ -0,0 +1,15 @@
+package com.example.admin.flagquizapp.Comon;
+
+public class Common {
+    public static final int EASY_MODE_NUM =30;
+    public static final int MEDIUM_MODE_NUM =50;
+    public static final int HARD_MODE_NUM =100;
+    public static final int HARDEST_MODE_NUM =200;
+
+    public enum MODE{
+        EASY,
+        MEDIUM,
+        HARD,
+        HARDEST
+    }
+}
